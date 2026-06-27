6a4cdefaa469dc8cf81e363181e405bd04271fb2
kezakethia2 <k.keza5@alustudent.com>
2026-06-27 18:56:11 +0200

    Complete Level 3 Quests 11 through 14

---
+++ quests/quest_12_the_two_path_cave.py
@@ -0,0 +1,8 @@
+s_password="OpenSeSame"
+
+User_input=(input("Enter your password: "))
+
+if User_input== s_password:
+    print("Access Granted")
+else:
+    print("Access denied")
---
+++ quests/quest_14_logical_gatekeeper.py
@@ -0,0 +1,8 @@
+age= int(input("Enter your age: "))
+gold_coins= int(input("Enter your Gold coins: "))
+
+if age >= 18 and  gold_coins >=20:
+    print("You can enter")
+else:
+    print("You cannot Enter")
+    
