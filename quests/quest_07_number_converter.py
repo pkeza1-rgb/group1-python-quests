25112541a0b8bf9cafdc734b401c23881cb69e48
Rebecca <r.isaboke@alustudent.com>
2026-06-28 11:08:52 -0700

    Add quest_07_number_converter.py quest

---
+++ quests/quest_07_number_converter.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Quest 7: The Magic Number Converter"""
+
+birth_year = int(input("What year were you born? "))
+age = 2026 - birth_year
+
+print(f"You are approximately {age} years old.")
