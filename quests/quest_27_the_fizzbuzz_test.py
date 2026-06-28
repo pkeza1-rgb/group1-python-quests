8e871b22cc33f0854f11f6b5f82e807a53e1abf1
adukwu137 <o.adukwu@alustudent.com>
2026-06-28 18:17:22 +0200

    all level 6 quests

---
+++ quests/quest_27_the_fizzbuzz_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def fizzbuzz_test():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+if __name__ == "__main__":
+    fizzbuzz_test()
